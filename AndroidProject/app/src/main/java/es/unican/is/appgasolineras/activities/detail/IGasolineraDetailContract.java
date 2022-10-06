08166a94e627d7f4c154ab2f22f57c55af8596df
Mario Martín Pérez <mmp819@alumnos.unican.es>
2022-10-06 02:42:13 +0200

    Completar versión inicial operativa de la vista detallada de una gasolinera.
    * Añadido cálculo de precio sumario.
    * Añadida obtención del horario de la gasolinera.
    * Añadida información básica restante de dirección.
    * Mejorada distribución de los layouts.
    * Incorporados nuevos strings.
    * Eliminados algunos imports innecesarios.

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/detail/IGasolineraDetailContract.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/detail/IGasolineraDetailContract.java
@@ -52,8 +52,38 @@ public interface IGasolineraDetailContract {
         void showName(String rotulo);
 
         /**
+         * Show direction of the Gas Station.
+         */
+        void showDirection(String direccion);
+
+        /**
          * Show name of the Gas Station's municipality.
          */
         void showMunicipality(String municipio);
+
+        /**
+         * Show CP of the Gas Station's municipality.
+         */
+        void showCP(String cp);
+
+        /**
+         * Show Gasolina95's price.
+         */
+        void showPrice95(String precio);
+
+        /**
+         * Show DieselA's price.
+         */
+        void showPriceDieselA(String precio);
+
+        /**
+         * Show schedule of the Gas Station.
+         */
+        void showSchedule(String horario);
+
+        /**
+         * Show summary of the Gas Station.
+         */
+        void showSummary(String sumario);
     }
 }
--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/model/Gasolinera.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/model/Gasolinera.java
@@ -27,7 +27,7 @@ public class Gasolinera implements Parcelable {
     @SerializedName("C.P.")                         private String cp;
     @SerializedName("Dirección")                    private String direccion;
     @SerializedName("Municipio")                    private String municipio;
-
+    @SerializedName("Horario")                      private String horario;
     @SerializedName("Precio Gasoleo A")             private String dieselA;
     @SerializedName("Precio Gasolina 95 E5")        private String normal95;  // 95 octanes
 
@@ -76,6 +76,14 @@ public class Gasolinera implements Parcelable {
         this.municipio = municipio;
     }
 
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
     public String getDieselA() {
         return dieselA;
     }
@@ -105,6 +113,7 @@ public class Gasolinera implements Parcelable {
         cp = in.readString();
         direccion = in.readString();
         municipio = in.readString();
+        horario = in.readString();
         dieselA = in.readString();
         normal95 = in.readString();
     }
@@ -133,6 +142,7 @@ public class Gasolinera implements Parcelable {
         dest.writeString(cp);
         dest.writeString(direccion);
         dest.writeString(municipio);
+        dest.writeString(horario);
         dest.writeString(dieselA);
         dest.writeString(normal95);
     }
