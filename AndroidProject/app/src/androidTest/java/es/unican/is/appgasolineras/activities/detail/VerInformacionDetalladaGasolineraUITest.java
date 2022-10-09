0b1e29c311649ee547f4865da156df7167df1d5c
Celso <cgc120@alumnos.unican.es>
2022-10-09 20:56:10 +0200

    cambio de nombre MostrarInformacionDetalladaGasolineraUITest a VerInformacion...

--- AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/detail/VerInformacionDetalladaGasolineraUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/detail/VerInformacionDetalladaGasolineraUITest.java
@@ -4,36 +4,26 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.anything;
 
 
-import android.view.View;
-
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Locale;
-
 import es.unican.is.appgasolineras.R;
-import es.unican.is.appgasolineras.activities.main.IMainContract;
 import es.unican.is.appgasolineras.activities.main.MainView;
-import es.unican.is.appgasolineras.model.Gasolinera;
-import es.unican.is.appgasolineras.repository.GasolinerasRepository;
-import es.unican.is.appgasolineras.repository.IGasolinerasRepository;
 import es.unican.is.appgasolineras.repository.rest.GasolinerasServiceConstants;
 
-public class MostrarInformacionGasolinerasUITest {
+public class VerInformacionDetalladaGasolineraUITest {
 
     @Rule
     public ActivityScenarioRule<MainView> activityRule =
@@ -44,21 +34,16 @@ public class MostrarInformacionGasolinerasUITest {
         GasolinerasServiceConstants.setStaticURL();
     }
 
-    /**@Before
-    public void initialize() {
-        mainView = new MainView();
-        detailView = new GasolineraDetailView();
-    }*/
-
     @AfterClass
     public static void clean() {
         GasolinerasServiceConstants.setMinecoURL();
     }
 
     @Test
-    public void mostrarInformacionGasolineraTest() {
-        onData(anything()).inAdapterView(withId(R.id.lvGasolineras)).atPosition(0).perform(click());
+    public void VerInformacionDetalladaGasolineraTest() {
 
+        // Datos correctos
+        onData(anything()).inAdapterView(withId(R.id.lvGasolineras)).atPosition(0).perform(click());
         onView(withId(R.id.tvDireccion)).check(matches(withText("CARRETERA 6316 KM. 10,5")));
         onView(withId(R.id.tvMunicipio)).check(matches(withText("Alfoz de Lloredo")));
         onView(withId(R.id.tvCP)).check(matches(withText("39526")));
@@ -66,13 +51,20 @@ public class MostrarInformacionGasolinerasUITest {
         onView(withId(R.id.tvDieselAPrecioDet)).check(matches(withText("1,999 €/L")));
         onView(withId(R.id.tvPrecioSumarioDet)).check(matches(withText("1,906 €/L")));
         onView(withId(R.id.tvHorarioDet)).check(matches(withText("L-D: 08:00-21:00")));
+
+        // Falta gasolina
+        onView(isRoot()).perform(ViewActions.pressBack());
+        onData(anything()).inAdapterView(withId(R.id.lvGasolineras)).atPosition(37).perform(click());
+        onView(withId(R.id.tvDireccion)).check(matches(withText("PASEO ESTRADA")));
+        onView(withId(R.id.tvMunicipio)).check(matches(withText("Comillas")));
+        onView(withId(R.id.tvCP)).check(matches(withText("39520")));
+        onView(withId(R.id.tv95PrecioDet)).check(matches(withText("- €/L")));
+        onView(withId(R.id.tvDieselAPrecioDet)).check(matches(withText("1,973 €/L")));
+        onView(withId(R.id.tvPrecioSumarioDet)).check(matches(withText("1,973 €/L")));
+        onView(withId(R.id.tvHorarioDet)).check(matches(withText("L-V: 09:00-19:00")));
+
+
     }
 
-    @Test
-    public void comprobarLogoGasolineraCorrectoTest() {
-        /**
-        onData(anything()).inAdapterView(withId(R.id.lvGasolineras)).atPosition(0).perform(click());
-        String idLogoMV = onView(withId(R.id.tvRotulo)).toString().toLowerCase(Locale.ROOT);
-        onView(withId(R.id.ivLogo)).check(matches(withId(R.drawable.cepsa)));*/
-    }
+
 }
