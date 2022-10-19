fc27f30a8eb7031bc71d92142db8e2db92ac9816
Celso <cgc120@alumnos.unican.es>
2022-10-19 23:20:42 +0200

    filtrar por tipo combustible interfaz test casi terminado

--- AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/main/filtrarPorTipoCombustibleUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/main/filtrarPorTipoCombustibleUITest.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -12,6 +13,9 @@ import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 
+import android.icu.text.UFormat;
+
+import androidx.test.espresso.DataInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.AfterClass;
@@ -47,11 +51,14 @@ public class filtrarPorTipoCombustibleUITest {
         onView(withId(R.id.spnTipoCombustible)).perform(click());
         //onView(allOf(withText("Diésel"))).perform(click());
         try {
-            Thread.sleep(5000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         onView(withId(R.id.tvApply)).perform(click());
+        DataInteraction firstGas = onData(anything()).inAdapterView(withId(R.id.lvGasolineras)).atPosition(1);
+        firstGas.onChildView(withId(R.id.tv95Label)).check(matches(withText("Gasolina:")));
+        firstGas.onChildView(withId(R.id.tv95Label)).check(matches(hasTextColor()));
 
 
 
