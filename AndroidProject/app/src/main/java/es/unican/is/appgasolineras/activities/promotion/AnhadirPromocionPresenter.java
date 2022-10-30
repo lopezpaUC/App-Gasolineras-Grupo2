17d343842e3def9c2dfc282104f714fdb48d2df4
Mario Martín Pérez <mmp819@alumnos.unican.es>
2022-10-30 21:42:38 +0100

    Completar funcionalidad para Añadir Promociones y ampliar Base de Datos.
    ** VIEW **
    * Permite mostrar un mensaje personalizado en función del error que pueda ocurrir al añadir una promoción.
    * En caso de éxito al añadir una promoción, se informa al usuario y se retorna a la actividad principal.
    
    ** PRESENTER **
    * Implementado método que añade las promociones con los datos obtenidos en la view y retorna información sobre si ha habido errores o no (y qué errores).
    
    ** REPOSITORIOS **
    * Añadidos nuevos métodos para actualizar tablas intermedias y obtener gasolinera o promociones relacionadas entre sí.
    
    ** DAO's **
    * Añadidas nuevas consultas para identificar gasolineras o promociones relacionadas entre sí.

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/promotion/AnhadirPromocionPresenter.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/promotion/AnhadirPromocionPresenter.java
@@ -1,110 +1,287 @@
 package es.unican.is.appgasolineras.activities.promotion;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
+import java.util.Map;
 
-import es.unican.is.appgasolineras.activities.main.CombustibleType;
 import es.unican.is.appgasolineras.model.Gasolinera;
 
+import es.unican.is.appgasolineras.model.Marca;
+import es.unican.is.appgasolineras.model.Promocion;
 import es.unican.is.appgasolineras.repository.IGasolinerasRepository;
+import es.unican.is.appgasolineras.repository.IPromocionesRepository;
 
+/**
+ * Presenter para la actividad relacionada con registrar una nueva promocion.
+ *
+ * @author Grupo 02-CarbuRed
+ * @version 1.0
+ */
 public class AnhadirPromocionPresenter implements IAnhadirPromocionContract.Presenter {
+
+    private static final String DESCUENTO_PORCENTAJE = "%";
+    private static final String DESCUENTO_EUROS_LITRO = "€/L";
+    private static final double ERROR_DESCUENTO = -1.0;
+
     // Vista
     private final IAnhadirPromocionContract.View view;
 
-
-
-    // Lista con todas las gasolineras
-    List<Gasolinera> data;
-
-    // Lista con el nombre y la dirección de todas las gasolineras
-    ArrayList<String> dataNombreDireccion;
+    // Repositorios
+    private IGasolinerasRepository repGasolineras;
+    private IPromocionesRepository repPromociones;
 
     /**
-     * Constructor
+     * Constructor del presenter de Anhadir Promociones.
+     *
      * @param view vista
      */
     public AnhadirPromocionPresenter(IAnhadirPromocionContract.View view) {
         this.view = view;
-
     }
 
     @Override
     public void init() {
-        dataNombreDireccion = new ArrayList<String>();
-        //repository = view.getGasolineraRepository();
-        rellenaGasolineras();
+        this.repGasolineras = view.getGasolineraRepository();
+        this.repPromociones = view.getPromocionRepository();
     }
-
-    /**
-     * Obtiene la lista con las gasolineras
-     */
-    private void rellenaGasolineras() {
-        /* TODO */
-    }
-
-
-    /**
-     * Anhade una promocion.
-     * @return true Si se ha podido anhadir. False en caso contrario.
-     */
 
     @Override
-    public boolean anhadePromocion(String n, String c, String m, String m2, double d, String e, List<String> gasolineras) {
+    public String[] getArrayGasolinerasParsed() {
+        List<Gasolinera> gasolineras = repGasolineras.getGasolineras();
+        Gasolinera g;
 
-        /*Promocion p;
+        String[] list = new String[gasolineras.size()];
 
-        // Convertir combustible
-        CombustibleType combustibleType = null;
-        c = c.toUpperCase(Locale.ROOT);
-        for(int i = 0; i < NUM_COMBUSTIBLES; i++ ) {
-            combustibleType.getCombTypeFromInt(i);
-            if(combustibleType.toString() == c) {
+        for (int i = 0; i < gasolineras.size(); i++) {
+            g = gasolineras.get(i);
+            list[i] = g.getRotulo() + " // " + g.getDireccion() + " // " + g.getMunicipio();
+        }
+
+        return list;
+    }
+
+    @Override
+    public EstadoOperacionAnhadirPromocion anhadirPromocion(Map<String, List<String>> infoList,
+                                                            Map<String, String> infoString) {
+
+        // Obtener ID de la promocion
+        String idPromocion = infoString.get("idPromocion");
+
+        // Comprobar si existe promocion con el mismo ID
+        if (repPromociones.getPromocionById(idPromocion) != null) {
+            return EstadoOperacionAnhadirPromocion.REPETIDA;
+        }
+
+        // Obtener combustibles seleccionados como String
+        @SuppressWarnings("ConstantConditions")
+        String combustiblesParsed = parseSelectedCombustibles(infoList.get("selectedCombustibles"));
+
+        // Comprobar que se seleccionaron combustibles
+        if (combustiblesParsed == null) {
+            return EstadoOperacionAnhadirPromocion.SIN_COMB;
+        }
+
+        // Obtener selecciones realizadas para una gasolinera o marcas.
+        Gasolinera gasolineraParsed;
+        List<Marca> marcasParsed;
+
+        gasolineraParsed = parseSelectedGasolinera(infoString.get("selectedGasolinera"));
+        //noinspection ConstantConditions
+        marcasParsed = parseSelectedMarcas(infoList.get("selectedMarcas"));
+
+        // Comprobar que se puede seleccionar alguna gasolinera
+        //noinspection ConstantConditions
+        if (gasolineraParsed == null && marcasParsed.isEmpty() &&
+                !infoString.get("selectedCriterio").equals(infoString.get("stringValueAllGas"))) {
+            return EstadoOperacionAnhadirPromocion.SIN_GASOLINERA;
+        }
+
+        // Comprobar que se ha introducido un descuento
+        String descuento = infoString.get("descuento");
+        if (descuento == null || descuento.equals("")) {
+            return EstadoOperacionAnhadirPromocion.SIN_DESC;
+        }
+
+        // Obtiene el valor de los descuentos en formato numerico
+        double descuentoPorcentual = parseDescuento(descuento);
+        double descuentoEurosLitro = descuentoPorcentual;
+
+        String tipoDescuento = infoString.get("selectedDescuentoTipo");
+        //noinspection ConstantConditions
+        switch (tipoDescuento) {
+            case DESCUENTO_EUROS_LITRO:
+                descuentoPorcentual = ERROR_DESCUENTO;
                 break;
+            case DESCUENTO_PORCENTAJE:
+                descuentoEurosLitro = ERROR_DESCUENTO;
+                break;
+            default:
+                break;
+        }
+
+        // Comprueba validez del descuento para el tipo escogido
+        if (tipoDescuento.equals(DESCUENTO_PORCENTAJE) &&
+                !checkDescuentoPorcentual(descuentoPorcentual)) {
+            return EstadoOperacionAnhadirPromocion.PORC_NO_VALIDO;
+        } else if (!checkDescuentoEuroLitro(descuentoEurosLitro)) {
+            return EstadoOperacionAnhadirPromocion.EURO_L_NO_VALIDO;
+        }
+
+        // Si los datos son correctos, persiste en base de datos
+        try {
+            Promocion p = new Promocion(idPromocion, descuentoPorcentual, descuentoEurosLitro,
+                    combustiblesParsed);
+            repPromociones.insertPromocion(p); // Registra promocion
+
+            if (gasolineraParsed != null) { // Si es para una gasolinera especifica
+                repPromociones.insertRelacionGasolineraPromocion(gasolineraParsed, p);
+
+            } else if (!marcasParsed.isEmpty()) { // Si es para un conjunto de marcas
+                insertMarcasPromocion(marcasParsed, p);
+            } else { // Si es para todas las gasolineras
+                insertGasolinerasPromocion(p);
             }
+        } catch (Exception e) {
+            return EstadoOperacionAnhadirPromocion.ERROR_BD;
         }
 
-        // Convertir porMarca
-        boolean porMarca;
-        if(m == "Por marca") {
-            porMarca = true;
-        } else {
-            porMarca = false;
-        }
-
-        // Convertir esPorcentaje
-        boolean esPorcentaje;
-        if(m == "%") {
-            esPorcentaje = true;
-        } else {
-            esPorcentaje = false;
-        }
-
-        *//* TODO convertir gasolineras y quitar el null *//*
-        List<Gasolinera> listaGasolineras = null;
-        // Crear la promocion
-        p = new Promocion(n, combustibleType, porMarca, m2, d, esPorcentaje, listaGasolineras);
-        aplicarPromocion(p);
-
-        *//* TODO llamar a la dao para anhadir promocion a la base de datos *//*
-        *//* TODO actualizar base de datos con los nuevos cambios en las gasolineras */
-
-        return true;
-
+        return EstadoOperacionAnhadirPromocion.EXITO;
     }
 
     /**
-     * Aplica la promocion en todas las gasolineras afectadas
-     * @param promocion
+     * Junta los combustibles seleccionados en un solo string en base a sus nombres.
+     * @param combustibles Lista de combustibles seleccionados.
+     * @return string con los combustibles seleccionados, separados por guiones.
      */
-    /* CELSO
-    private void aplicarPromocion(Promocion promocion) {
-        for(Gasolinera gasolinera: promocion.getGasolineras()) {
-            gasolinera.aplicarPromocion(promocion);
+    private String parseSelectedCombustibles(List<String> combustibles) {
+        // String nulo si no se seleccionaron combustibles
+        if (combustibles.isEmpty()) {
+            return null;
         }
+
+        StringBuilder builder = new StringBuilder();
+        Iterator<String> iterator = combustibles.iterator();
+        builder.append(iterator.next());
+
+        while (iterator.hasNext()) {
+            builder.append("-");
+            builder.append(iterator.next());
         }
-    }*/
+
+        return builder.toString();
+    }
+
+    /**
+     * Transforma una lista de nombres de marcas, en una lista de marcas representadas como
+     * clases que reflejen la entidad en la base de datos.
+     *
+     * @param marcas Lista de nombres de marcas.
+     * @return Lista de marcas como clases.
+     */
+    private List<Marca> parseSelectedMarcas(List<String> marcas) {
+        List<Marca> mParsed = new ArrayList<>();
+
+        for (String m:marcas) {
+            mParsed.add(new Marca(m));
+        }
+
+        return mParsed;
+    }
+
+    /**
+     * Obtiene de un string correspondiente al nombre, direccion y localidad de una gasolinera,
+     * la gasolinera que representa.
+     *
+     * @param g String que representa gasolinera: Formato -> nombre // dir // localidad
+     * @return Gasolinera a la que hace referencia el string.
+     */
+    private Gasolinera parseSelectedGasolinera(String g) {
+        if (g!= null && !g.equals("-")) {
+            String[] info = g.split(" // ");
+            return repGasolineras.getGasolineraByNameDirLocalidad(info[0], info[1], info[2]);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Convierte un descuento representado como String, a un descuento como valor decimal.
+     *
+     * @param descuentoStr Descuento en formato de cadena de caracteres.
+     * @return descuento como valor decimal, o -1.0 en caso de ocurrir un error.
+     */
+    private double parseDescuento(String descuentoStr) {
+        double descuento;
+
+        try {
+            descuento = Double.parseDouble(descuentoStr);
+        } catch (Exception e) {
+            descuento = ERROR_DESCUENTO;
+        }
+
+        return descuento;
+    }
+
+    /**
+     * Comprueba si el descuento porcentual es valido.
+     *
+     * @param descuento Descuento porcentual.
+     * @return true si es valido.
+     *         false en caso contrario.
+     */
+    private boolean checkDescuentoPorcentual(double descuento) {
+        boolean validez;
+
+        if (descuento == ERROR_DESCUENTO) { // Si no se convirtio correctamente
+            validez = false;
+        } else {
+            validez = descuento <= 100.0 && descuento > 0.0;
+        }
+
+        return validez;
+    }
+
+    /**
+     * Comprueba si el descuento en euros/litro es valido.
+     *
+     * @param descuento Descuento en euros/litro.
+     * @return true si es valido.
+     *         false en caso contrario.
+     */
+    private boolean checkDescuentoEuroLitro(double descuento) {
+        boolean validez;
+
+        if (descuento == ERROR_DESCUENTO) { // Si no se convirtio correctamente
+            validez = false;
+        } else {
+            validez = descuento > 0.0;
+        }
+
+        return validez;
+    }
+
+    /**
+     * Inserta marcas y su relacion con la promocion
+     * @param marcasParsed Marcas relacionadas con la promocion
+     * @param p Promocion
+     */
+    private void insertMarcasPromocion(List<Marca> marcasParsed, Promocion p) {
+        for (Marca m : marcasParsed) {
+            repPromociones.insertMarca(m);
+            repPromociones.insertRelacionMarcaPromocion(m, p);
+        }
+    }
+
+    /**
+     * Inserta todas las relaciones de las gasolineras con la promocion.
+     * @param p Promocion
+     */
+    private void insertGasolinerasPromocion(Promocion p) {
+        for (Gasolinera g : repGasolineras.getGasolineras()) {
+            repPromociones.insertRelacionGasolineraPromocion(g, p);
+        }
+    }
 }
 
 
